1c1da047d2f5657b75ec49468a97fb4c9cfc109e
Jannevhooff <janne.hooff@me.com>
2017-05-01 10:51:15 +0200

    Add files via upload

---
+++ beweging.js
@@ -0,0 +1,12 @@
+/* element beweegbaar maken */
+
+$( function() {
+        $( ".balk" ).draggable();
+    } );
+
+/* verandere van style door te klikken */
+
+//$('.balk').click(function() {
+//    $(".balk").removeClass('balk');
+//    $(".twee").addClass('twee');
+//});
